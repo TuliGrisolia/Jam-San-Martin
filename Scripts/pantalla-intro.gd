2a16ee5234adbf59101db304293dab4618671e5e
TuliGrisolia <100006715+TuliGrisolia@users.noreply.github.com>
2023-10-01 19:25:38 -0300

    intro-menu-credito_sin-implementar

---
+++ Scripts/pantalla-intro.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("Intro_Sonido").play()
+	get_node("AnimationPlayer").play("Animación1")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	pass # Replace with function body.
+
+
+func _on_animation_player_animation_finished(anim_name):
+	pass # Replace with function body.
