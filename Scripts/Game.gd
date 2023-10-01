c516b449cbf1a9fd9ad39a22ed939731903f0df6
TuliGrisolia <100006715+TuliGrisolia@users.noreply.github.com>
2023-09-30 21:58:42 -0300

    Snake-en-escena

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -4,11 +4,13 @@ const SNAKE = 0
 const FOOD = 2
 
 var food_pos
+var snake_body = [Vector2i(10,10), Vector2i(9,10), Vector2i(8,10)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	food_pos = def_pos_food()
 	add_food()
+	add_snake()
 	pass # Replace with function body.
 
 func def_pos_food():
@@ -20,4 +22,9 @@ func def_pos_food():
 	
 func add_food():
 	$SnakeFood.set_cell(0, Vector2i(food_pos.x, food_pos.y), 2, Vector2i(0,0))
+	pass
+
+func add_snake():
+	for part in snake_body:
+		$SnakeFood.set_cell(0, Vector2i(part.x, part.y), 0, Vector2i(0,0))
 	pass
