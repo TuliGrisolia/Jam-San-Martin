a4ea604eb842d9ddaa2adf9cf55adbbd9bae4905
TuliGrisolia <100006715+TuliGrisolia@users.noreply.github.com>
2023-10-02 16:00:39 -0300

    Pantalla intro - lista

--- Scripts/pantalla-intro.gd
+++ Scripts/pantalla-intro.gd
@@ -9,7 +9,6 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Intro_Sonido").play()
-	get_node("AnimationPlayer").play("Animación1")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,10 +16,6 @@ func _ready():
 #	pass
 
 
-func _on_AnimationPlayer_animation_finished(anim_name):
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
-	pass # Replace with function body.
-
-
-func _on_animation_player_animation_finished(anim_name):
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://Escenas/menu.tscn")
 	pass # Replace with function body.
