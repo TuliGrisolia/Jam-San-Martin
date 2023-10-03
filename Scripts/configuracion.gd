dc660f1bf3381a7d005c6859fcb254ab701b5e58
TuliGrisolia <100006715+TuliGrisolia@users.noreply.github.com>
2023-10-03 00:59:27 -0300

    Música-Sonidos-ok

--- Scripts/configuracion.gd
+++ Scripts/configuracion.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+func _on_h_slidersonido_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sonido"), value)
+	pass # Replace with function body.
+
+
+func _on_h_sliderefectos_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Efectos"), value)
+	pass # Replace with function body.
 
 
 func _on_button_pressed():
